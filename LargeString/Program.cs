2d767535a000d628fc454dcd4da4b104f6742cdc
fcoa <felixcoa13@hotmail.com>
2023-01-28 20:53:22 -0300

    Add project files.

---
+++ LargeString/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LargeString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
